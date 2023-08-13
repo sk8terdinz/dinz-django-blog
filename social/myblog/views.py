0748c63a3530139ba3b5406a9cf4ed4d2be0857c
sk8terdinz <d.refranto@gmail.com>
2023-08-14 01:40:23 +0700

    adding profile, profile list, follow, unfollow

--- social/myblog/views.py
+++ social/myblog/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Profile
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {"profiles":profiles})
+    else:
+        messages.success(request, ("You Must Be Logged In To View This Page..."))
+        return redirect('home')
+    
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+
+        # Post Form Logic
+        if request.method == "POST":
+            # Get current user
+            current_user_profile = request.user.profile
+            # Get form data
+            action = request.POST['follow']
+            # Decide to follow or unfollow
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+            # Save the profile
+            current_user_profile.save()
+                
+        return render(request, "profile.html", {"profile":profile})
+    else:
+        messages.success(request, ("You Must Be Logged In To View This Page..."))
+        return redirect('home')
